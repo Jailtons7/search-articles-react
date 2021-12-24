6231ce9b4a880d5052feca5acfe5d599516f36fb
Jailton Silva <jss1@ifal.edu.br>
2021-12-24 16:28:24 -0300

    feat: first adding first routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import PrivateRoute from './components/login/PrivateRoute';
+import Home from './components/Home';
+import Login from './components/login/Login';
+import NotFoundError from './components/NotFoundError';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute path="/" exact component={ Home } />
+        <Route path="/login" exact component={ Login } />
+        <Route component={ NotFoundError } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
