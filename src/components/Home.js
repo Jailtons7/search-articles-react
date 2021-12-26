941175fe83cce450833f36bd77389fe222b64edb
Jailton Silva <jss1@ifal.edu.br>
2021-12-26 15:17:12 -0300

    feat: first version of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
+import Menu from './painel/Menu';
+import Search from './painel/Search';
+import Articles from './painel/Articles';
+
 const Home = () => {
+  const articles = [
+    {
+      id: "1",
+      title: "Microsserviços e a web 3.0",
+      autor: "Jailton de Souza Silva",
+      resumo: "PT - EN",
+      idioma: "PT",
+    },
+    {
+      id: "2",
+      title: "Python como alternativa para um desenvolvimento ágil",
+      autor: "Jailton de Souza Silva",
+      resumo: "PT - EN",
+      idioma: "PT",
+    },
+    {
+      id: "3",
+      title: "Software livre e a segurança na tecnologia da informação",
+      autor: "Jailton de Souza Silva",
+      resumo: "PT - EN",
+      idioma: "PT",
+    }
+  ]
   return ( 
-    <h1>Home page</h1>
+    <div className='container-fluid'>
+      <div className='row'>
+        <Menu />
+        <div className='col'>
+          <Search />
+          <Articles articles={articles}/>
+        </div>
+      </div>
+    </div>
   );
 }
- 
+
 export default Home;
