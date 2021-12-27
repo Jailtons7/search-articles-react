80cbfcd5bc70c316078e390230907d25588b9239
Jailton Silva <jss1@ifal.edu.br>
2021-12-27 16:45:44 -0300

    feat: remove Login.css file

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import SearchArticlesLogo from './SearchArticleLogo'
-import './Login.css'
 
 const Login = () => {
   return ( 
