ff4d6c8a23b3c4a70101b36b50e3a4b6959f3a24
Jailton Silva <jss1@ifal.edu.br>
2021-12-27 16:38:24 -0300

    feat: adding more articles

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,21 +8,35 @@ const Home = () => {
   const articles = [
     {
       id: "1",
-      title: "Microsserviços e a web 3.0",
+      title: "A crise mundial de semicondutores e preço do hardware no Brasil",
       autor: "Jailton de Souza Silva",
       resumo: "PT - EN",
       idioma: "PT",
     },
     {
       id: "2",
-      title: "Python como alternativa para um desenvolvimento ágil",
+      title: "Microsserviços e a web 3.0",
       autor: "Jailton de Souza Silva",
       resumo: "PT - EN",
       idioma: "PT",
     },
     {
       id: "3",
+      title: "Python como alternativa para um desenvolvimento ágil",
+      autor: "Jailton de Souza Silva",
+      resumo: "PT - EN",
+      idioma: "PT",
+    },
+    {
+      id: "4",
       title: "Software livre e a segurança na tecnologia da informação",
+      autor: "Jailton de Souza Silva",
+      resumo: "PT - EN",
+      idioma: "PT",
+    },
+    {
+      id: "5",
+      title: "Os desafios que o mundo precisa enfrentar na formação de desenvolvedores",
       autor: "Jailton de Souza Silva",
       resumo: "PT - EN",
       idioma: "PT",
