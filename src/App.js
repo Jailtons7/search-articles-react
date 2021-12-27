1a3fda6e1471f5134436037fe0ebd07aa84eae0c
Jailton Silva <jss1@ifal.edu.br>
2021-12-27 16:37:36 -0300

    feat: adding routes create-account and edit-profile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/login/PrivateRoute';
 import Home from './components/Home';
 import Login from './components/login/Login';
+import CreateAccount from './components/login/CreateAccount';
+import EditProfile from './components/login/EditProfile';
 import NotFoundError from './components/NotFoundError';
 
 import './App.css';
@@ -14,6 +16,8 @@ function App() {
       <Switch>
         <PrivateRoute path="/" exact component={ Home } />
         <Route path="/login" exact component={ Login } />
+        <Route path="/account-create" exact component={ CreateAccount } />
+        <PrivateRoute path="/edit-profile" component={ EditProfile } />
         <Route component={ NotFoundError } />
       </Switch>
     </BrowserRouter>
