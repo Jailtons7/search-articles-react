a42123e9b3648a158a9eed629c50b703791918c2
Jailton Silva <jss1@ifal.edu.br>
2022-01-12 10:20:16 -0300

    feat: getting data from web api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Menu from './painel/Menu';
 import Search from './painel/Search';
 import Articles from './painel/Articles';
+import axios from 'axios';
 
 const Home = () => {
-  const articles = [
-    {
-      id: "1",
-      title: "A crise mundial de semicondutores e preço do hardware no Brasil",
-      autor: "Jailton de Souza Silva",
-      resumo: "PT - EN",
-      idioma: "PT",
-    },
-    {
-      id: "2",
-      title: "Microsserviços e a web 3.0",
-      autor: "Jailton de Souza Silva",
-      resumo: "PT - EN",
-      idioma: "PT",
-    },
-    {
-      id: "3",
-      title: "Python como alternativa para um desenvolvimento ágil",
-      autor: "Jailton de Souza Silva",
-      resumo: "PT - EN",
-      idioma: "PT",
-    },
-    {
-      id: "4",
-      title: "Software livre e a segurança na tecnologia da informação",
-      autor: "Jailton de Souza Silva",
-      resumo: "PT - EN",
-      idioma: "PT",
-    },
-    {
-      id: "5",
-      title: "Os desafios que o mundo precisa enfrentar na formação de desenvolvedores",
-      autor: "Jailton de Souza Silva",
-      resumo: "PT - EN",
-      idioma: "PT",
+  const [articles, setArticles] = useState({
+    count: 0,
+    next: null,
+    prev: null,
+    results: [
+      {
+        CodArtigoPublicado: 0,
+        ART: 0,
+        TipArtigoRelevancia: null,
+        TipArtigoDivulgacaoCientifica: null,
+        AnoPublicacaoArtigo: null,
+        CodSiglaAtuacaoProfissionalLattes: {},
+        CodIssn: {},
+        CodTitulosArtigos: {},
+        CodIdioma: {},
+        CodServidor: {},
+      }
+    ]
+  })
+
+  useEffect(() => {
+    const fetchArticles = async (url) => {
+      const {data} = await axios.get(
+        "http://localhost:8000/api/v1/articles/"
+      )
+      setArticles(data)
     }
-  ]
+
+    fetchArticles()
+  }, [])
+
   return ( 
     <div className='container-fluid'>
       <div className='row'>
         <Menu />
         <div className='col'>
-          <Search />
-          <Articles articles={articles}/>
+          <Search articles={articles} setArticles={setArticles}/>
+          <Articles articles={articles.results}/>
         </div>
       </div>
     </div>
