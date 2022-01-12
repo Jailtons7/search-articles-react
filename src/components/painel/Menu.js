7675c2726f215666e587326ec4aa530995363b4d
Jailton Silva <jss1@ifal.edu.br>
2022-01-12 10:28:54 -0300

    feat: changes application logo

--- src/components/painel/Menu.js
+++ src/components/painel/Menu.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import searchArticle from '../../img/search_articles_t.png'
+import searchArticle from '../../img/logo_search_articles.png'
 
 const Menu = () => {
   return (
     <>
       <div className='col-auto col-md-2 px-0 bg-secondary'>
         <div className='ms-0 row w-100 bg-white d-none d-sm-block'>
-          <img height="60px" src={searchArticle} alt="Logo" />
+          <Link to='/' >
+            <img height="60px" src={searchArticle} alt="Logo" />
+          </Link>
+        </div>
+        <div className='flex-nowrap my-4 text-center'>
+          <i className="bi bi-person text-white"></i>
+          <span className='ms-3 text-white'>User Name</span>
         </div>
         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-          <Link to='/' className='text-white d-flex align-items-center pb-3 mb-md-0 me-md-auto text-write text-decoration-none'>
-            <span className='fs-5 d-none d-sm-inline'>Home</span>
-          </Link>
           <ul className='nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start' id='menu'>
             <li className='nav-item'>
               <Link to='/edit-profile' className='text-white nav-link align-middle px-0'>
