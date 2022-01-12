7f52ea4a2de4db41c5a64e7e138a0d3db6149926
Jailton Silva <jss1@ifal.edu.br>
2022-01-12 10:27:52 -0300

    feat: filtering articles from web api

--- src/components/painel/Filtro.js
+++ src/components/painel/Filtro.js
@@ -1,23 +1,41 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-const Filtro = () => {
+const Filtro = (props) => {
   const [placeholder, setPlaceholder] = useState("Título da publicação")
+  const [nameInput, setNameInput] = useState("titulo")
 
   const handleSelect = (e) => {
     setPlaceholder(e.target.options[e.target.selectedIndex].text);
+    setNameInput(e.target.value)
   }
 
-  return ( 
+  const fetchArticles = async (url) => {
+    const {data} = await axios.get(
+      url
+    )
+    props.setArticles(data)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form_ = document.getElementById("articleSearch")
+
+    fetchArticles('http://localhost:8000/api/v1/articles/?' + form_.name + '=' + form_.value)
+  }
+
+  return (
+    <form method='get' onSubmit={handleSubmit}>
     <div className='mt-4 mb-5 ms-2'>
       <div className='row'>
         <div className='col-md-6'>
-          <input type='text' className='form-control' id='articleTitle' name='articleTitle'
+          <input type='text' className='form-control' id='articleSearch' name={nameInput}
                 placeholder={placeholder} />
         </div>
         <div className='col-md-3'>
-          <select className='form-select' id='indices' name='indices'
+          <select className='form-select text-secondary' id='indices' name='indices'
                   onChange={handleSelect}>
-            <option value='título' selected>Título da publicação</option>
+            <option value='titulo' selected>Título da publicação</option>
             <option value='ano'>Ano da publicação</option>
             <option value='autor'>Autor da publicação</option>
           </select>
@@ -30,6 +48,7 @@ const Filtro = () => {
         </div>
       </div>
     </div>
+    </form>
   );
 }
 
