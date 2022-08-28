dd484d229fd96ce79e1fd50762df8cdc5d16e02f
Jailton Silva <jss1@ifal.edu.br>
2022-08-28 11:48:44 -0300

    change Article component

--- src/components/painel/Article.js
+++ src/components/painel/Article.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link } from 'react-router-dom';
 
 const Article = ({article}) => {
   return (
     <div className='col mx-2'>
       <hr className='text-danger me-1'></hr>
-      <Link className='mb-2 text-decoration-none' to=''>{article.CodTitulosArtigos.DscTitulosArtigos}</Link>
+      <Link to={{pathname: '/articles/' + article.CodArtigoPublicado}} className='mb-2 text-decoration-none'>{article.CodTitulosArtigos.DscTitulosArtigos}</Link>
       <p className='mb-2 text-secondary'>{article.CodServidor.NomServidor}</p>
       <p className='mb-2 text-secondary'>
-        <strong>Instituição</strong>: {article.CodSiglaAtuacaoProfissionalLattes.CodSiglaOrgaoIE} | 
-        <strong>Idioma</strong>: {article.CodIdioma.DscIdioma} | 
+        <strong>Instituição</strong>: {article.CodSiglaAtuacaoProfissionalLattes.CodSiglaOrgaoIE} <span className='ms-2 me-2'>|</span>
+        <strong>Idioma</strong>: {article.CodIdioma.DscIdioma} <span className='ms-2 me-2'>|</span>
         <strong>Ano</strong>: {article.AnoPublicacaoArtigo}
       </p>
     </div>
