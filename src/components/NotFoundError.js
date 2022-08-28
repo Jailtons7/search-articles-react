4c5e130d6089b7ba7a379847a1712e65ed4402a4
Jailton Silva <jss1@ifal.edu.br>
2022-08-28 11:50:51 -0300

    Add not found error component

--- src/components/NotFoundError.js
+++ src/components/NotFoundError.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Menu from './painel/Menu';
+
 const NotFoundError = () => {
   return (
     <>
-      <h1>404: NotFondError.</h1>
-      <h3>
-        Esta página não existe
-      </h3>
-      Volte para a nossa Home Page: <Link to="/">Home</Link>
+      <div className='container-fluid'>
+        <div className='row'>
+          <Menu />
+          <div className='col align-self-center'>
+            <div className='row justify-content-sm-center'>
+              <div className='col-sm-4'>
+                <h1>404: NotFoundError.</h1>
+                <h3>
+                  Esta página não existe
+                </h3>
+                Volte para a nossa Home Page: <Link to="/">Home</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
